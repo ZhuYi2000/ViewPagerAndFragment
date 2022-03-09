de07d7f7e295fa8257fc1813ba556420696a9985
zhuyi_y7000 <1739677792@qq.com>
2022-03-09 20:02:08 +0800

    测试了远程连接云数据库

--- app/src/main/java/com/example/viewpagerandfragment/MainActivity.java
+++ app/src/main/java/com/example/viewpagerandfragment/MainActivity.java
@@ -5,7 +5,15 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 //viewpager2使用了懒加载
 public class MainActivity extends AppCompatActivity {
@@ -29,5 +37,35 @@ public class MainActivity extends AppCompatActivity {
     public void openMapActivity(View view) {
         Intent map_intent = new Intent(this,MapActivity.class);
         startActivity(map_intent);
+    }
+
+    public void testOnlineDatabase(View view) {
+        //不能在主线程中调用，必须要在新的线程中申请数据库
+        new Thread(){
+            @Override
+            public void run(){
+                String url = "jdbc:mysql://rm-bp1bnehb104c22283fo.mysql.rds.aliyuncs.com/pokemon?useUnicode=true&amp;characterEncoding=utf-8";
+                String user = "p_database";
+                String password = "Yige1234";
+                String driver = "com.mysql.jdbc.Driver";
+                try {
+                    Class.forName(driver);
+                    Connection conn = DriverManager.getConnection(url,user,password);
+                    Statement st = conn.createStatement();
+                    ResultSet rs = st.executeQuery("select * from user_base");
+                    while (rs.next()){
+                        //解决在子线程中调用Toast的异常情况处理
+                        Looper.prepare();
+                        Toast.makeText(MainActivity.this, "你好，"+rs.getString("user_name"), Toast.LENGTH_SHORT).show();
+                        Looper.loop();
+                    }
+                    rs.close();
+                    st.close();
+                    conn.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
     }
 }
